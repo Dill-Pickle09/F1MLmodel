7fbfde6e8fd949799cdfd7f8562a718093238e68
Dill-Pickle09 <dylanbpatel@outlook.com>
2025-11-26 09:03:45 -0800

    App Updates
    Reincorporated XGBoost model for user to select in app.

--- app.py
+++ app.py
@@ -2,22 +2,63 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load("models/rf_model.pkl")
+rf_model = joblib.load("models/rf_model.pkl")
+xgb_model = joblib.load("models/xgb_model.pkl")
 
-st.title("F1 Race Position Predictor")
-st.write("Enter race details to predict finishing position:")
+drivers_df = pd.read_csv("data/drivers.csv")
+constructors_df = pd.read_csv("data/constructors.csv")
 
-driver_id = st.number_input("Driver ID", value=1)
-constructor_id = st.number_input("Constructor ID", value=1)
-grid_position = st.number_input("Grid Position", value=10)
+driver_options = dict(zip(drivers_df["surname"], drivers_df['driverId']))
+constructor_options = dict(zip(constructors_df["name"], constructors_df["constructorId"]))
 
-if st.button("Predict"):
-    input_data = pd.DataFrame([{
-        "driverId": driver_id,
-        "constructorId": constructor_id,
+st.set_page_config(page_title="F1 Race Position Predictor", layout="centered")
+
+st.title("F1 Race Position Predictor!")
+st.markdown(
+    """
+    Welcome to the **F1 Machine Learning Prediction App**!
+    Select a **driver**, **constructor**, and **grid position**, and the model will predict the finishing position.
+    """
+)
+
+st.divider()
+
+model_choice = st.selectbox(
+    "Choose ML Model:",
+    ["Random Forest", "XGBoost"]
+)
+
+driver_name = st.selectbox("Driver:", list(driver_options.keys()))
+constructor_name = st.selectbox("Constructor:", list(constructor_options.keys()))
+
+grid_position = st.slider("Grid Position (Starting Position)", 1, 20, 10)
+
+st.divider()
+
+if st.button("Predict FInishing Position", use_container_with=True):
+
+    chosen_driver = driver_options[driver_name]
+    chosen_constructor = constructor_options[constructor_name]
+
+    input_data = pd.DataFram([{
+        "driverId": chosen_driver,
+        "constructorId": chosen_constructor,
         "grid": grid_position
     }])
 
+    model = rf_model if model_choice == "Random Forest" else xgb_model
+
     prediction = model.predict(input_data)[0]
 
-    st.success(f"Predicted finishing position: **{prediction:2f}**")
+    st.success(f"**Predicted finishing position: `{prediction: .2f}`**")
+
+    if prediction < grid_position:
+        st.markdown("**The model predicts the driver will finish above the starting position.**")
+    elif prediction > grid_position:
+        st.markdown("**The model predicts the driver may drop positions in the race.**")
+    else:
+        st.markdown("**The model predicts the driver will finish in the same spot they started.**")
+
+    st.divider()
+
+
